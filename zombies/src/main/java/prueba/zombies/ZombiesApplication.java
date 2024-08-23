e1b53b70d832476f7d7f475ea23317b1f5e4db0b
Arancha Fernandez <aferfer1978@gmail.com>
2024-08-23 09:26:27 +0200

    dependencias y estructura inicial

---
+++ zombies/src/main/java/prueba/zombies/ZombiesApplication.java
@@ -0,0 +1,13 @@
+package prueba.zombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombiesApplication.class, args);
+	}
+
+}
---
+++ zombies/src/test/java/prueba/zombies/ZombiesApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.zombies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
